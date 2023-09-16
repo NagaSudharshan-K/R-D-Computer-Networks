7151d6449f296d95780b9b3af1213ffe824dda45
NagaSudharshan-K <nagas2003@gmail.com>
2023-09-16 22:55:57 +0530

    tracks web applications

--- process_tracker.py
+++ process_tracker.py
@@ -1,9 +1,12 @@
 import psutil
 
-def list_processes():
+def list_web_app_processes():
+    web_browsers = ["chrome", "firefox", "msedge"]  
     for process in psutil.process_iter(attrs=['pid', 'name']):
-        print(f"Process Name: {process.info['name']}, PID: {process.info['pid']}")
+        process_name = process.info['name'].lower()
+        for browser in web_browsers:
+            if browser in process_name:
+                print(f"Web Application Name: {process_name}, PID: {process.info['pid']}")
 
 if __name__ == "__main__":
-    list_processes()
-
+    list_web_app_processes()
